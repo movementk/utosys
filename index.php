676a9fe2f780ced7613ce23cf4ab2ddceb7842c8
Joahra <mk_ahra0522@gmail.com>
2018-07-12 18:35:34 +0900

    유토시스 메인 헤더 작업중...

---
+++ index.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="ko">
+	<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="/assets/fontello/css/fontello.css">
+	<link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/assets/css/default.css">
+
+	<title>유토시스</title>
+	</head>
+	<body>
+		<header id="header">          
+			<div class="container">
+				<h1 class="logo">
+					<a href="#">
+						<img src="/assets/images/logo.jpg" alt="유토시스 로고">
+					</a>
+				</h1>
+				<!--<nav id="gnb">
+					<ul>
+						<li>
+							<a href="#"></a>
+						</li>
+					</ul>
+				</nav>-->
+				<div class="service">
+					<ul class="laguage">
+						<li></li>
+					</ul>
+					<div class="search-form">
+						<form action="#">
+							<!--input type="text"-->
+							<button type="submit" class="btn search-btn">
+								<span class="sr-only">검색버튼</span>
+								<i class="icon-search"></i>
+							</button>
+						</form>
+					</div>
+				</div>
+				<div class="nav-btn-area">
+					<button type="button" class="btn btn-nav">
+						<i class="sr-only">메뉴열기</i>
+						<span class="bar"></span>
+						<span class="bar"></span>
+						<span class="bar"></span>
+					</button>
+				</div>
+			</div>
+		</header>
+		<main id="content">
+			<div class="container">
+				hello world
+			</div>
+		</main>
+		<footer id="footer">
+			<div class="container">
+				hello world
+			</div>
+		</footer>
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+		<script src="/assets/bootstrap/js/bootstrap.min.js"></script>
+	</body>
+</html>
