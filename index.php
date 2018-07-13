ba8a5eb462f7f50a33e0c421babd379a7bda1b27
Joahra <mk_ahra0522@gmail.com>
2018-07-13 17:31:46 +0900

    헤더 푸터 완료.

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="/assets/fontello/css/fontello.css">
 	<link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/assets/css/flags/flag-icon.css">
 	<link rel="stylesheet" href="/assets/css/default.css">
 
 	<title>유토시스</title>
@@ -18,38 +19,6 @@
 						<img src="/assets/images/logo.jpg" alt="유토시스 로고">
 					</a>
 				</h1>
-				<div class="d-none d-xl-block">
-					<nav id="gnb">
-						<ul>
-							<li>
-								<a href="#">회사소개</a>
-							</li>
-							<li>
-								<a href="#">사업영역</a>
-							</li>
-							<li>
-								<a href="#">인재채용</a>
-							</li>
-							<li>
-								<a href="#">고객센터</a>
-							</li>
-						</ul>
-					</nav>
-				</div>
-				<div class="service">
-					<ul class="laguage">
-						<li></li>
-					</ul>
-					<div class="search-form">
-						<form action="#">
-							<!--input type="text"-->
-							<button type="submit" class="btn search-btn">
-								<span class="sr-only">검색버튼</span>
-								<i class="icon-search"></i>
-							</button>
-						</form>
-					</div>
-				</div>
 				<div class="nav-btn-area">
 					<button type="button" class="btn btn-nav">
 						<i class="sr-only">메뉴열기</i>
@@ -57,6 +26,71 @@
 						<span class="bar"></span>
 						<span class="bar"></span>
 					</button>
+				</div>
+				<nav id="gnb">
+					<ul class="menu">
+						<li>
+							<a href="#">회사소개</a>
+							<ul class="gnb2">
+								<li><a href="#">회사소개</a></li>
+								<li><a href="#">회사소개</a></li>
+								<li><a href="#">회사소개</a></li>
+							</ul>
+						</li>
+						<li>
+							<a href="#">사업영역</a>
+							<ul class="gnb2">
+								<li><a href="#">사업영역</a></li>
+								<li><a href="#">사업영역</a></li>
+								<li><a href="#">사업영역</a></li>
+							</ul>
+						</li>
+						<li>
+							<a href="#">인재채용</a>
+							<ul class="gnb2">
+								<li><a href="#">ddd</a></li>
+								<li><a href="#">ddd</a></li>
+								<li><a href="#">ddd</a></li>
+							</ul>
+						</li>
+						<li>
+							<a href="#">고객센터</a>
+							<ul class="gnb2">
+								<li><a href="#">ddd</a></li>
+								<li><a href="#">ddd</a></li>
+								<li><a href="#">ddd</a></li>
+							</ul>
+						</li>
+					</ul>
+					<ul class="laguage">
+						<li>
+							<a href="#">
+								<span class="flag-icon flag-icon-kr"></span>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<span class="flag-icon flag-icon-us"></span>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<span class="flag-icon flag-icon-cn"></span>
+							</a>
+						</li>
+					</ul>
+				</nav>
+				<div class="global-form">
+					<form action="#">
+						<!--div class="form-group">
+							<input type="text" class="form-control">
+						</div-->
+						<button type="submit" class="btn search-btn">
+							<i class="icon-search">
+								<span class="sr-only">검색버튼</span>
+							</i>
+						</button>
+					</form>
 				</div>
 			</div>
 		</header>
@@ -69,7 +103,53 @@
 		</main>
 		<footer id="footer">
 			<div class="container">
-				hello world
+				<div class="row">
+					<div class="col-12 col-xl-10">
+						<ul class="policy">
+							<li>
+								<a href="#">회사소개</a>
+							</li>
+							<li>
+								<a href="#">찾아오시는 길</a>
+							</li>
+							<li>
+								<a href="#">인재채용</a>
+							</li>
+							<li>
+								<a href="#">문의하기</a>
+							</li>
+						</ul>
+						<div class="company">
+							<ul>
+								<li>경북 구미시 해평면 도신로 162-32 (주)유토시스</li>
+								<li>
+									<dl>
+										<dt>TEL </dt>
+										<dd>054-473-8122</dd>
+									</dl>
+								</li>
+								<li>
+									<dl>
+										<dt>FAX</dt>
+										<dd>054-473-8123</dd>
+									</dl>
+								</li>
+								<li>
+									<dl>
+										<dt>대표자</dt>
+										<dd>백차흠</dd>
+									</dl>
+								</li>
+							</ul>
+							<p>COPYRIGHT (C) 2018 UTOSYS.  ALL RIGHTS RESERVED. SITE BY MOVEMENT K GROUP.</p>
+						</div>
+					</div>
+					<div class="d-none d-xl-block col-xl-2">
+						<figure>
+							<img src="/assets/images/img_footer_logo.png" alt="">
+						</figure>
+					</div>
+				</div>
 			</div>
 		</footer>
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
