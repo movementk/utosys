323a0bcad6a5e8544144d6e9c58030de0cda997f
Joahra <mk_ahra0522@gmail.com>
2018-07-19 14:36:20 +0900

    sub수정

---
+++ sub.php
@@ -0,0 +1,50 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link rel="stylesheet" href="/assets/css/sub.css">
+</head>
+<body class="sub">
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/aside.php'); ?>
+    
+	<!-- page-header -->
+    <div class="page-header">
+        <div class="page-summary">
+            <h2>회사소개</h2>
+        </div>
+    </div>
+    
+    <!-- page-path -->
+	<div class="page-path">
+		<div class="container">
+			<ol class="breadcrumb">
+				<li class="home">
+					<a href="/"> 
+						<span class="sr-only">메인이동</span>
+					</a>
+				</li>
+				<li>
+					<div class="dropdown">
+						<button class="btn btn-secondary dropdown-toggle" type="button" id="depth-list-1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							인사말
+						</button>
+						<div class="dropdown-menu" aria-labelledby="depth-list-1">
+							<a class="dropdown-item" href="#">경영이념</a>
+							<a class="dropdown-item" href="#">연혁/사업실적</a>
+							<a class="dropdown-item" href="#">조직도</a>
+							<a class="dropdown-item" href="#">인증현황</a>
+						</div>
+					</div>
+				</li>
+			</ol>
+		</div>
+	</div>
+	
+	
+	<!-- Content -->
+    <main id="content" class="">
+    	
+	</main>
+		
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+</body>
+</html>
