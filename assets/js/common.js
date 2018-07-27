528faf68146564600ac784ce9d1e1a3009f40d48
kimyh5392 <mk.kimyh@gmail.com>
2018-07-27 13:43:45 +0900

    js 변경 완료.

--- assets/js/common.js
+++ assets/js/common.js
@@ -63,13 +63,13 @@
     // global-search
     $(document).on('click','.global-search .btn-search', function(){
         $('.global-search').addClass('action');
-        $('.form-group').addClass('d-block');
-        $('.form-group').removeClass('d-none');
+        $('.global-search .form-group').addClass('d-block');
+        $('.global-search .form-group').removeClass('d-none');
     });
     $(document).on('click','#content, #gnb, #header .container', function(){
         $('.global-search').removeClass('action');
-        $('.form-group').removeClass('d-block');
-        $('.form-group').addClass('d-none');
+        $('.global-search .form-group').removeClass('d-block');
+        $('.global-search .form-group').addClass('d-none');
     });
 	
 })(jQuery);
