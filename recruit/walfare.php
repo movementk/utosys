b993bb3118553a8c189b036c4665cfbeafd3db6b
Joahra <mk_ahra0522@gmail.com>
2018-07-27 11:04:08 +0900

    복지제도 작업중...

---
+++ recruit/walfare.php
@@ -0,0 +1,50 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link rel="stylesheet" href="/assets/css/sub.css">
+<link rel="stylesheet" href="/assets/css/recruit.css">
+</head>
+<body>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/aside.php'); ?>
+    
+	<!-- page-header -->
+    <div class="page-header">
+        <div class="page-summary">
+            <h2>인재채용</h2>
+        </div>
+		<!-- page-path -->
+		<div class="page-path">
+			<div class="container"> 
+				<ol class="breadcrumb"> 
+					<li></li> 
+					<li> 
+						<div class="dropdown">
+							<button class="btn btn-secondary dropdown-toggle" type="button" id="depth-list" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+								인재상
+							</button>
+							<div class="dropdown-menu" aria-labelledby="depth-list">
+								<a class="dropdown-item" href="/recruit/recruit_person.php">인재상</a>
+								<a class="dropdown-item active" href="/recruit/walfare.php">복지제도</a>
+								<a class="dropdown-item" href="#">채용안내</a>
+							</div> 
+						</div>
+					</li>
+				</ol>
+			</div>
+		</div>
+    </div>
+   
+	<!-- Content -->
+    <main id="content" class="walfare">
+    	<div class="container">
+			<!-- page-title -->
+			<div class="page-title">
+				<h2>복지제도</h2>
+			</div>
+			
+		</div>	
+	</main>
+		
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+</body>
+</html>
